76270cdf29a0a944993fb698ee1a295a4e6a0fe7
Leblanc Meneses <leblancmeneses@gmail.com>
2013-05-01 12:52:01 -0500

    adding minification process - missed previously

--- Languages/npeg_javascript/_Build.bat
+++ Languages/npeg_javascript/_Build.bat
@@ -1,4 +1,4 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build\combine-compress.proj /fl /flp:v=diag;logfile=build.log
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe tools\combine-compress.proj /fl /flp:v=diag;logfile=build.log
 pause
